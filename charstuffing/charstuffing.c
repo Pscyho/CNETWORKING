b1979c78fd42b73cc95dfb9c7be26fd9590032cc
Nagendra <nx2k3@duck.com>
2025-08-11 19:56:06 +0530

    add char stuffing program

---
+++ charstuffing/charstuffing.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#define FLAG 'F'
+#define ESC 'E'
+
+int main() {
+  char data[100], stuffed[200];
+  int i, j = 0;
+  printf("enter the data string(without spaces):\n");
+  scanf("%s", data);
+  stuffed[j++] = FLAG;
+  for (i = 0; i < strlen(data); i++) {
+    if (data[i] == FLAG) {
+      stuffed[j++] = ESC;
+    }
+    stuffed[j++] = data[i];
+  }
+  stuffed[j++] = FLAG;
+  stuffed[j] = '\0';
+  printf("the origial data is: %s", data);
+  printf("\nthe stuffed data is: %s\n", stuffed);
+  return 0;
+}
